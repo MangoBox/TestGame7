c3f790675b74b1b547a8055ebf644136e3a08243
MangoBox <mangobox228@hotmail.com.au>
2016-11-15 18:03:21 +1100

    Alright another commit just to test things out :D

--- Assets/Scripts/NewRunManager.cs
+++ Assets/Scripts/NewRunManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class NewRunManager : MonoBehaviour {
 
-
+    //BLAH BLAH BLAH
     //The primary method for beginning a new run. Should not contain too many fleshed out methods, only calling global ones.
     public void BeginNewRun()
     {
